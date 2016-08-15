dbc80688620572e83c8c23138ec41de7b29bc4ef
kritikgarg <kritikgarg240194@gmail.com>
2016-08-16 00:18:46 +0530

    Final

---
+++ assignment1/ques2.sql
@@ -0,0 +1,6 @@
+-- Write a SELECT command to display information on the books
+-- that have not been returned till date. The information should
+-- include book issue date, title, member name and due date.
+-- Note: Use Correlated Subquery.
+
+-- Unable to underStand 
